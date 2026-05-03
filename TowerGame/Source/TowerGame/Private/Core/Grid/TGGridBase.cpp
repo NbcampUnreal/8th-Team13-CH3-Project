80b106e4466bb1117509870b60f3dd80df4a158d
Serbin2 <njh1026@gmail.com>
2026-05-03 19:05:46 +0900

    FEAT : BFS로 경로 탐색 후 건설 가능 여부 확인 함수 추가

--- TowerGame/Source/TowerGame/Private/Core/Grid/TGGridBase.cpp
+++ TowerGame/Source/TowerGame/Private/Core/Grid/TGGridBase.cpp
@@ -1,4 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Core/Grid/TGGridBase.h"
@@ -10,30 +10,105 @@ ATGGridBase::ATGGridBase()
 	PrimaryActorTick.bCanEverTick = false;
 }
 
-ATGSingleGrid* ATGGridBase::GetSingleGridFromPosition(FVector Position)
+ATGSingleGrid* ATGGridBase::GetSingleGridFromPosition(const FVector Position)
 {
-	FVector2D pos = FVector2D(Position.X, Position.Y);
-	return GetSingleGridFromPosition(pos);
+	FVector2D Pos = FVector2D(Position.X, Position.Y);
+	return GetSingleGridFromPosition(Pos);
 }
 
-ATGSingleGrid* ATGGridBase::GetSingleGridFromPosition(FVector2D position)
+ATGSingleGrid* ATGGridBase::GetSingleGridFromPosition(const FVector2D Position)
 {
-	int row = position.X / GridSize;
-	int col = position.Y / GridSize;
-	if (row < 0 || row >= GridRow || col < 0 || col >= GridCol)	return nullptr;
-	
-	return GridActors[row * GridCol + col];
+	int X = Position.X / GridSize;
+	int Y = Position.Y / GridSize;
+	if (X < 0 || X >= GridX || Y < 0 || Y >= GridY)	return nullptr;
+
+	return GridActors[Y][X];
+}
+
+FIntPoint ATGGridBase::GetPointFromPosition(const FVector Position) const
+{
+	FVector2D Pos = FVector2D(Position.X, Position.Y);
+	return GetPointFromPosition(Pos);
+}
+
+FIntPoint ATGGridBase::GetPointFromPosition(const FVector2D Position) const
+{
+	int X = Position.X / GridSize;
+	int Y = Position.Y / GridSize;
+	if (X < 0) X = 0;
+	else if (X >= GridX) X = GridX - 1;
+	if (Y < 0) Y = 0;
+	else if (Y >= GridY) Y = GridY - 1;
+	return FIntPoint(X, Y);
+}
+
+bool ATGGridBase::CanBePlacedBuilding(FIntPoint Point)
+{
+	bBuildingPlaced[Point.Y][Point.X] = true;
+
+	//	1. 진입점이 막혀있으면 안됨
+	//	2. 도착점이 막혀있으면 안됨
+	bool bBlocked = true;
+	for (int32 i = 0; i < GridX; i++)
+	{
+		if (!bBuildingPlaced[0][i])
+		{
+			bBlocked = false;
+			break;
+		}
+		else if (!bBuildingPlaced[GridY - 1][i])
+		{
+			bBlocked = false;
+			break;
+		}
+	}
+
+	if (bBlocked)
+	{	//	진입점 또는 진출점이 막히게 됨
+		bBuildingPlaced[Point.Y][Point.X] = false;
+		return false;
+	}
+
+	//	3. 경로가 막혀있으면 안됨
+	if (!PathFinding())
+	{
+		bBuildingPlaced[Point.Y][Point.X] = false;
+		return false;
+	}
+
+	return true;
+}
+
+void ATGGridBase::PlacingBuilding(FIntPoint Point)
+{
+	bBuildingPlaced[Point.Y][Point.X] = true;
+
+	if (Point.Y == 0)
+	{
+		EntryPoints.Remove(Point);
+	}
+	if (Point.Y == GridY - 1)
+	{
+		ExitPoints.Remove(Point);
+	}
 }
 
 void ATGGridBase::BeginPlay()
 {
 	Super::BeginPlay();
 
-	GridActors.Reserve(GridRow * GridCol);
-	for (int32 i = 0; i < GridRow * GridCol; i++)
+	GridActors.SetNum(GridY);
+	bBuildingPlaced.SetNum(GridY);
+	for (int32 i = 0; i < GridY; i++)
 	{
-		ATGSingleGrid* NewGrid = GetWorld()->SpawnActor<ATGSingleGrid>();
-		GridActors.Add(NewGrid);
+		GridActors[i].Reserve(GridX);
+		bBuildingPlaced[i].Reserve(GridX);
+		for (int32 j = 0; j < GridX; j++)
+		{
+			ATGSingleGrid* NewGrid = GetWorld()->SpawnActor<ATGSingleGrid>();
+			GridActors[i].Add(NewGrid);
+			bBuildingPlaced[i].Add(false);
+		}
 	}
 	PlacingGrid();
 }
@@ -41,17 +116,80 @@ void ATGGridBase::BeginPlay()
 void ATGGridBase::PlacingGrid()
 {
 	FVector OriginPos = GetActorLocation();
-	
-	for (int32 i = 0; i < GridCol; i++)
+
+	for (int32 i = 0; i < GridY; i++)
 	{
-		for (int32 j = 0; j < GridRow; j++)
+		for (int32 j = 0; j < GridX; j++)
 		{
-			ATGSingleGrid* SingleGrid = GridActors[j * GridCol + i];
+			ATGSingleGrid* SingleGrid = GridActors[i][j];
 			FVector GridPos = OriginPos;
 			GridPos.X += (GridSize * j) + (GridSize / 2);
 			GridPos.Y += (GridSize * i) + (GridSize / 2);
 			SingleGrid->SetActorLocation(GridPos);
+			if (i == 0)
+			{	//	진입점
+				EntryPoints.Add(FIntPoint(j, i));
+			}
+			else if (i == GridY - 1)
+			{	//	진출점
+				ExitPoints.Add(FIntPoint(j, i));
+			}
 		}
 	}
 }
 
+bool ATGGridBase::PathFinding()
+{	//	경로간 비용이 없으므로 BFS로 패스파인딩
+	//	맵이 과도하게 커질 경우 비효율 적일 수 있으나 현재 수준에선 적절하다고 판단됨
+	//	방문 배열 생성
+	TArray<TArray<bool>> Visited;
+	Visited.SetNum(GridY);
+	for (int32 i = 0; i < GridY; i++)
+	{
+		Visited[i].Init(false, GridX);
+	}
+
+	//	모든 열린 진입점을 동시에 출발점으로 추가 (멀티소스 BFS)
+	TArray<FIntPoint> Queue;	//	탐색큐
+	for (const FIntPoint& Entry : EntryPoints)
+	{
+		if (!bBuildingPlaced[Entry.Y][Entry.X])
+		{
+			Visited[Entry.Y][Entry.X] = true;
+			Queue.Add(Entry);
+		}
+	}
+
+	//	탐색 방향
+	const int32 DX[] = { 1, -1, 0, 0 };
+	const int32 DY[] = { 0, 0, 1, -1 };
+
+	for (int32 Head = 0; Head < Queue.Num(); Head++)
+	{	
+		const FIntPoint Current = Queue[Head];
+
+		if (ExitPoints.Contains(Current))
+		{	//	탐색큐의 위치가 도착점
+			return true;
+		}
+
+		//	각 방향 확인
+		for (int32 d = 0; d < 4; d++)
+		{
+			const int32 NX = Current.X + DX[d];
+			const int32 NY = Current.Y + DY[d];
+
+			//	인덱스 벗어남
+			if (NX < 0 || NX >= GridX || NY < 0 || NY >= GridY) continue;
+			//	이미 방문 or 막힌 곳
+			if (Visited[NY][NX] || bBuildingPlaced[NY][NX]) continue;
+
+			//	방문한 위치는 큐에 넣기
+			Visited[NY][NX] = true;
+			Queue.Add(FIntPoint(NX, NY));
+		}
+	}
+
+	//	탐색 완료 했으나 길찾기 실패
+	return false;
+}
--- TowerGame/Source/TowerGame/Public/Core/Grid/TGGridBase.h
+++ TowerGame/Source/TowerGame/Public/Core/Grid/TGGridBase.h
@@ -1,4 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -16,28 +16,45 @@ class TOWERGAME_API ATGGridBase : public ATGActorBase
 public:
 	ATGGridBase();
 
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="TowerGame|Grid")
+	ATGSingleGrid* GetSingleGridFromPosition(const FVector Position);
+	ATGSingleGrid* GetSingleGridFromPosition(const FVector2D Position);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="TowerGame|Grid")
+	FIntPoint GetPointFromPosition(const FVector Position) const;
+	FIntPoint GetPointFromPosition(const FVector2D Position) const;
+
 	UFUNCTION(BlueprintCallable, Category="TowerGame|Grid")
-	ATGSingleGrid* GetSingleGridFromPosition(FVector Position);
-	ATGSingleGrid* GetSingleGridFromPosition(FVector2D position);
-	
+	bool CanBePlacedBuilding(FIntPoint Point);
+
+	UFUNCTION(BlueprintCallable, Category="TowerGame|Grid")
+	void PlacingBuilding(FIntPoint Point);
+
 protected:
 	virtual void BeginPlay() override;
-	
+
 	//	각 그리드 크기에 맞게 싱글그리드들을 배치합니다
 	//	원점을 전체 그리드 맵의 모서리로 합니다.
 	void PlacingGrid();
-	
-	//	그리드 맵의 크기입니다.
+
+	//	그리드 맵의 X축 칸 수입니다.
 	UPROPERTY(EditDefaultsOnly, Category="TowerGame|Grid")
-	int GridRow;
+	int GridX;
+	//	그리드 맵의 Y축 칸 수입니다.
 	UPROPERTY(EditDefaultsOnly, Category="TowerGame|Grid")
-	int GridCol;
-	
+	int GridY;
+
 	//	각 그리드의 크기입니다
 	UPROPERTY(EditDefaultsOnly, Category="TowerGame|Grid")
 	float GridSize;
+
+private:
 	
-	UPROPERTY(VisibleAnywhere, Category="TowerGame|Grid")
-	TArray<ATGSingleGrid*> GridActors;
+	bool PathFinding();
 	
+	TArray<TArray<bool>> bBuildingPlaced;
+	TArray<TArray<ATGSingleGrid*>> GridActors;
+	TSet<FIntPoint> EntryPoints;
+	TSet<FIntPoint> ExitPoints;
+
 };
