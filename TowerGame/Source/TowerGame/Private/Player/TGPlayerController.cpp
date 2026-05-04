88c6da77574df1f138b4c1dd25aa76fb023175b7
khalicika-dev <khalicika@gmail.com>
2026-05-04 20:15:33 +0900

    add PlayerCharacter, PlayerController, GameModeBase

--- TowerGame/Source/TowerGame/Private/Player/TGPlayerController.cpp
+++ TowerGame/Source/TowerGame/Private/Player/TGPlayerController.cpp
@@ -2,4 +2,40 @@
 
 
 #include "Player/TGPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
 
+ATGPlayerController::ATGPlayerController()
+{
+	static ConstructorHelpers::FObjectFinder<UInputMappingContext> IMC_Asset =
+		TEXT("/Game/Player/Input/IMC_Player.IMC_Player");
+	if (IMC_Asset.Succeeded())
+		InputMappingContext = IMC_Asset.Object;
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> IA_Move_Asset =
+		TEXT("/Game/Player/Input/Actions/IA_Move.IA_Move");
+	if (IA_Move_Asset.Succeeded())
+		Action_Move = IA_Move_Asset.Object;
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> IA_Look_Asset =
+		TEXT("/Game/Player/Input/Actions/IA_Look.IA_Look");
+	if (IA_Look_Asset.Succeeded())
+		Action_Look = IA_Look_Asset.Object;
+}
+
+void ATGPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (InputMappingContext)
+			{
+				Subsystem->AddMappingContext(InputMappingContext, 0);
+			}
+		}
+	}
+}
